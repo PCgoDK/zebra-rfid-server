f096456e5c03004173543a10c2ac162d53a7d0eb
pcgoDK <ulf@pcgo.dk>
2026-09-02 16:26:13 +0200

    Open Swagger in current tab

--- tests/test_main.py
+++ tests/test_main.py
@@ -53,6 +53,7 @@ def test_dashboard_renders_login_and_reader_controls() -> None:
     assert 'id="home"' in body
     assert 'id="swagger"' in body
     assert 'href="/docs"' in body
+    assert 'id="swagger" class="btn btn-outline-light btn-sm d-none" href="/docs">' in body
 
 
 def test_login_returns_a_token_only_for_valid_credentials() -> None:
